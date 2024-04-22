d98d7bddfe55825e45d2bf51c164c6d0a108ee0b
Jane <hezha.aban@gmail.com>
2024-04-22 21:23:05 +0200

    till minut 3:21

---
+++ WebApp/Controllers/CoursesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers;
+
+[Authorize]
+public class CoursesController(HttpClient httpClient) : Controller
+{
+
+    private readonly HttpClient _httpClient = httpClient;
+
+    public async Task<IActionResult> Index()
+    {
+        var viewModel = new CourseIndexViewModel();
+        
+        //hämtar våra kurser
+        var response = await _httpClient.GetAsync("https://localhost:7276/api/Courses");
+        if (response.IsSuccessStatusCode)
+        {
+            var courses = JsonConvert.DeserializeObject<IEnumerable<CourseViewModel>> (await response.Content.ReadAsStringAsync());
+            if (courses != null  && courses.Any())
+                viewModel.Course = courses;
+
+        }
+
+        return View(viewModel);
+
+
+    }
+}
