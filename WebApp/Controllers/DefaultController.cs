ceef4761dc60a73ecb3c7ceade186215813adb0f
Jane <hezha.aban@gmail.com>
2024-04-24 00:23:47 +0200

    Fungerar med subscribed, already subscribed : 4:05:30

--- WebApp/Controllers/DefaultController.cs
+++ WebApp/Controllers/DefaultController.cs
@@ -5,20 +5,40 @@ using WebApp.ViewModels;
 
 namespace WebApp.Controllers;
 
-public class DefaultController(HttpClient httpClient) : Controller
+public class DefaultController : Controller
 {
+    private readonly HttpClient _httpClient;
+
+    public DefaultController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
     public IActionResult Home()
     {
         return View();
     }
 
     [HttpPost]
-    
+
     public async Task<IActionResult> Subscribe(SubscribeViewModel model)
     {
-        if (ModelState.IsValid) 
+        if (ModelState.IsValid)
         {
             var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("https://localhost:7276/api/subscribe", content);
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["StatusMessage"] = "You are now subscribed";
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Conflict)
+            {
+                TempData["StatusMessage"] = "You are already subscribed";
+            }
+        }
+        else
+        {
+            TempData["StatusMessage"] = "Invalid email address";
         }
         return RedirectToAction("Home", "Default", "subscribe");
     }
